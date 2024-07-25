c98a0536d93e5d1241ab7c118eec0653e99ab788
Arsenkats <kats45mam@gmail.com>
2024-07-26 00:42:25 +0300

    add i files

---
+++ src/js/1-timer.js
@@ -0,0 +1,20 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const dateInput = document.querySelector('input');
+console.log(dateInput);
+
+const options = {
+  enableTime: true,
+  dateFormat: "Y-m-d H:i",
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(dateInput, {});
